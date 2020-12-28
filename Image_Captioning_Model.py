528071c6b591f568b1ca66d6da73b0a5aa5dff84
AtharvaParanjpe <atharvapparanjpe@gmail.com>
2020-12-28 09:51:00 +0530

    First Commit

---
+++ Image_Captioning_Model.py
@@ -0,0 +1,148 @@
+from numpy import array
+from pickle import load
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.utils import plot_model
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Embedding
+from keras.layers import Dropout
+from keras.layers.merge import add
+from keras.callbacks import ModelCheckpoint
+
+# load doc into memory
+def load_doc(filename):
+	file = open(filename, 'r')
+	text = file.read()
+	file.close()
+	return text
+
+# load a pre-defined list of photo identifiers
+def load_set(filename):
+	doc = load_doc(filename)
+	dataset = list()
+	for line in doc.split('\n'):
+		if len(line) < 1:
+			continue
+		identifier = line.split('.')[0]
+		dataset.append(identifier)
+	return set(dataset)
+
+# load clean descriptions into memory
+def load_clean_descriptions(filename, dataset):
+	doc = load_doc(filename)
+	descriptions = dict()
+	for line in doc.split('\n'):
+		tokens = line.split()
+		image_id, image_desc = tokens[0], tokens[1:]
+		if image_id in dataset:
+			if image_id not in descriptions:
+				descriptions[image_id] = list()
+			desc = 'startseq ' + ' '.join(image_desc) + ' endseq'
+			descriptions[image_id].append(desc)
+	return descriptions
+
+# load photo features
+def load_photo_features(filename, dataset):
+	all_features = load(open(filename, 'rb'))
+	features = {k: all_features[k] for k in dataset}
+	return features
+
+# covert a dictionary of clean descriptions to a list of descriptions
+def to_lines(descriptions):
+	all_desc = list()
+	for key in descriptions.keys():
+		[all_desc.append(d) for d in descriptions[key]]
+	return all_desc
+
+# fit a tokenizer given caption descriptions
+def create_tokenizer(descriptions):
+	lines = to_lines(descriptions)
+	tokenizer = Tokenizer()
+	tokenizer.fit_on_texts(lines)
+	return tokenizer
+
+# calculate the length of the description with the most words
+def max_length(descriptions):
+	lines = to_lines(descriptions)
+	return max(len(d.split()) for d in lines)
+
+# create sequences of images, input sequences and output words for an image
+def create_sequences(tokenizer, max_length, desc_list, photo, vocab_size):
+	X1, X2, y = list(), list(), list()
+	for desc in desc_list:
+		seq = tokenizer.texts_to_sequences([desc])[0]
+		for i in range(1, len(seq)):
+			in_seq, out_seq = seq[:i], seq[i]
+			in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
+			out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
+			X1.append(photo)
+			X2.append(in_seq)
+			y.append(out_seq)
+	return array(X1), array(X2), array(y)
+
+# define the captioning model
+def define_model(vocab_size, max_length):
+	inputs1 = Input(shape=(4096,))
+	fe1 = Dropout(0.5)(inputs1)
+	fe2 = Dense(256, activation='relu')(fe1)
+	inputs2 = Input(shape=(max_length,))
+	se1 = Embedding(vocab_size, 256, mask_zero=True)(inputs2)
+	se2 = Dropout(0.5)(se1)
+	se3 = LSTM(256)(se2)
+	decoder1 = add([fe2, se3])
+	decoder2 = Dense(256, activation='relu')(decoder1)
+	outputs = Dense(vocab_size, activation='softmax')(decoder2)
+	model = Model(inputs=[inputs1, inputs2], outputs=outputs)
+	model.compile(loss='categorical_crossentropy', optimizer='adam')
+	model.summary()
+	#plot_model(model, to_file='model.png', show_shapes=True)
+	return model
+
+# data generator, intended to be used in a call to model.fit_generator()
+def data_generator(descriptions, photos, tokenizer, max_length, vocab_size):
+	# loop for ever over images
+	while 1:
+		for key, desc_list in descriptions.items():
+			# retrieve the photo feature
+			photo = photos[key][0]
+			in_img, in_seq, out_word = create_sequences(tokenizer, max_length, desc_list, photo, vocab_size)
+			yield [in_img, in_seq], out_word
+
+# load training dataset (6K)
+filename = 'Flickr8k_text/Flickr_8k.trainImages.txt'
+train = load_set(filename)
+print('Dataset: %d' % len(train))
+# descriptions
+train_descriptions = load_clean_descriptions('descriptions.txt', train)
+print('Descriptions: train=%d' % len(train_descriptions))
+# photo features
+train_features = load_photo_features('features.pkl', train)
+print('Photos: train=%d' % len(train_features))
+# prepare tokenizer
+tokenizer = create_tokenizer(train_descriptions)
+vocab_size = len(tokenizer.word_index) + 1
+print('Vocabulary Size: %d' % vocab_size)
+# determine the maximum sequence length
+max_length = max_length(train_descriptions)
+print('Description Length: %d' % max_length)
+
+# define the model
+model = define_model(vocab_size, max_length)
+# train the model, run epochs manually and save after each epoch
+epochs = 20
+steps = len(train_descriptions)
+for i in range(epochs):
+	generator = data_generator(train_descriptions, train_features, tokenizer, max_length, vocab_size)
+	# print(generator)
+	# input()
+	model.fit(generator, epochs=1, steps_per_epoch=steps, verbose=1)
+	model.save('./models/model_' + str(i) + '.h5')
+	
+
+
+
+## Model Evaluation
---
+++ Readme.md
@@ -0,0 +1,1 @@
+# For detailed description view the blog at : https://machinelearningmastery.com/develop-a-deep-learning-caption-generation-model-in-python/
